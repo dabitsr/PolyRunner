5c09671541bed309057eecbecd4e12663a613b0f
dabitsr <43137289+dabitsr@users.noreply.github.com>
2021-11-17 20:38:33 +0100

    Players collection!

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerFloor : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    Animator anim;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
-        anim.Play("Waving");
+        transform.position = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position;
     }
 }
--- Assets/Scripts/PlayersGeneration.cs
+++ Assets/Scripts/PlayersGeneration.cs
@@ -9,18 +9,16 @@ public class PlayersGeneration : MonoBehaviour
 
     Vector3 floorSize;
     Vector3 platformSize;
-    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
         firstPlatformPos = GameObject.FindGameObjectWithTag("Respawn").transform.position;
         print(firstPlatformPos);
         platformSize = GetComponent<Renderer>().bounds.size;
         floorSize = new Vector3(firstPlatformPos.x + platformSize.x/2, firstPlatformPos.y, firstPlatformPos.z + (platformSize.z * transform.childCount));
         print(floorSize);
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 40; i++)
         {
             Vector3 p = new Vector3(Random.Range(firstPlatformPos.x, floorSize.x), floorSize.y, Random.Range(firstPlatformPos.z, floorSize.z));
             Instantiate(playerPrefab, p, Quaternion.Euler(new Vector3(0, 180, 0)));
@@ -31,14 +29,5 @@ public class PlayersGeneration : MonoBehaviour
     void Update()
     {
             
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        print(collision.gameObject.name);
-        if (collision.gameObject.tag == "Player")
-        {
-            print("AAA");
-            anim.Play("Running");
-        }
     }
 }
