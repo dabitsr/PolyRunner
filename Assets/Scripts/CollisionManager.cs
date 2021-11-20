19b26209ff6e63b2df2c4679478989b18576326d
dabitsr <43137289+dabitsr@users.noreply.github.com>
2021-11-20 20:08:42 +0100

    update

---
+++ Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionManager : MonoBehaviour
+{
+    Slider playerCounterSlider;
+    PlayerScript playerScript;
+    List<int> pickedAllies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = GameObject.Find("Player").GetComponent<PlayerScript>();
+        playerCounterSlider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("FloorPlayer"))
+        {
+            // Update UI
+            GameObject.Find("PlayersCounterUI").GetComponent<PlayersCollectedScript>().increasePlayersCollected();
+
+            playerCounterSlider.value += 1 / 24.0f;
+
+            playerScript.collectAlly();
+        }
+    }
+
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float speed, maxLeft, maxRight;
+    GameObject player;
+    Vector2 posRelativeToPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (gameObject.CompareTag("Ally"))
+            player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * 5, transform.position.y + Input.GetAxis("Vertical") * 5, transform.position.z);
+        if (gameObject.CompareTag("Player")) movePlayer();
+        else moveAlly();
+    }
+
+    void moveAlly()
+    {
+        Vector3 move = Vector3.zero;
+        // Offsets del jugador respecto a la posición que debería tener
+        float xOffset = (transform.position.x - player.transform.position.x) - posRelativeToPlayer.x;
+        float zOffset = (transform.position.z - player.transform.position.z) - posRelativeToPlayer.y;
+
+        move.x = Input.GetAxis("Horizontal");
+        if (transform.position.x >= maxRight)
+        {
+            transform.position = new Vector3(maxRight, transform.position.y, transform.position.z);
+            if (xOffset > 0) move.x += (xOffset * -1);
+            if (move.x > 0) move.x = 0;
+        }
+        else if (transform.position.x <= maxLeft)
+        {
+            transform.position = new Vector3(maxLeft, transform.position.y, transform.position.z);
+            if (xOffset < 0) move.x += (xOffset * -1);
+            if (move.x < 0) move.x = 0;
+        }
+        else if (xOffset != 0 || zOffset != 0)
+        {
+            move.x += (xOffset * -1);
+            move.z += (zOffset * -1);
+        }
+        transform.Translate(new Vector3(move.x * speed * Time.deltaTime, 0, (speed + speed * move.z) * Time.deltaTime));
+    }
+
+    void movePlayer()
+    {
+        float move = 0;
+        move = Input.GetAxis("Horizontal");
+        if (transform.position.x >= maxRight && move > 0) move = 0;
+        else if (transform.position.x <= maxLeft && move < 0) move = 0;
+        transform.Translate(new Vector3(move * speed * Time.deltaTime, 0, speed * Time.deltaTime));
+    }
+
+    public void setPosRelativeToPlayer(Vector2 newPos)
+    {
+        posRelativeToPlayer = newPos;
     }
 }
