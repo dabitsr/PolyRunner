5405437e3f316d929d9af811eb6dcb845fa81b0e
aalexisp <alexis.peralta@estudiantat.upc.edu>
2021-11-27 14:49:03 +0100

    Camera animations ended

--- Assets/Scripts/MainMenu/StartGame.cs
+++ Assets/Scripts/MainMenu/StartGame.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class StartGame : MonoBehaviour
 {
+    public GameObject westTrain;
+    public GameObject scifiBalloon;
+    public GameObject cityBalloon;
+
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        if (sceneName == "PolyRunnerFirstScene")
+        {
+            westTrain.transform.position += westTrain.transform.right * Time.deltaTime * 2;
+        }
+
+        if (westTrain.transform.position.x > 90) SceneManager.LoadScene(sceneName);
     }
 }
