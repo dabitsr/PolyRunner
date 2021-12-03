201fd14d4a5af1e87bbcb05880277775885795a8
dabitsr <43137289+dabitsr@users.noreply.github.com>
2021-12-03 20:14:12 +0100

    1st map update

--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -6,6 +6,7 @@ public class ShipController : MonoBehaviour
 {
     [SerializeField] Transform player;
     [SerializeField] float distance, force;
+    [SerializeField] ParticleSystem hitParticle;
 
     Rigidbody rb;
     // Start is called before the first frame update
@@ -17,14 +18,17 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rb.velocity.magnitude < 15)
+        if (rb.velocity.magnitude < 15 && player.position.z >= transform.position.z - distance)
             Move();
-        //if (player.position.z >= transform.position.z - distance)
-            
     }
 
     void Move()
     {
         rb.AddRelativeForce(Vector3.forward * force * Time.deltaTime, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        hitParticle.Play();
     }
 }
