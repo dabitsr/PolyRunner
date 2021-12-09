a32153c4f01ada49fdb3a5df93f1d0c7adefa736
dabitsr <43137289+dabitsr@users.noreply.github.com>
2021-12-09 10:56:43 +0100

    2nd map update

---
+++ Assets/FlameThrowerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThrowerController : MonoBehaviour
+{
+    [SerializeField] ParticleSystem flame;
+    [SerializeField] float flameTime, preFlameTime;
+
+    int currentFlame = -1, nextFlame = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(NextFlame(preFlameTime));
+        InvokeRepeating("StartFlame", preFlameTime, flameTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartFlame() => StartCoroutine(StartFlameCoroutine());
+
+    IEnumerator StartFlameCoroutine()
+    {
+        if (currentFlame != -1)
+        {
+            transform.GetChild(currentFlame).GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(currentFlame).GetChild(0).gameObject.GetComponent<BoxCollider>().tag = "Untagged";
+        }
+
+        transform.GetChild(nextFlame).GetChild(1).gameObject.SetActive(false);
+
+        currentFlame = nextFlame;
+        transform.GetChild(currentFlame).GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(currentFlame).GetChild(0).gameObject.GetComponent<BoxCollider>().tag = "Obstacle";
+
+        StartCoroutine(NextFlame(flameTime - preFlameTime));
+        yield return new WaitForSeconds(flameTime);
+    }
+
+    IEnumerator NextFlame(float s)
+    {
+        yield return new WaitForSeconds(s);
+
+        int n;
+        do
+        {
+            n = Random.Range(0, transform.childCount);
+        } while (n == currentFlame);
+
+        nextFlame = n;
+
+        // Light
+        transform.GetChild(nextFlame).GetChild(1).gameObject.SetActive(true);
+    }
+}
